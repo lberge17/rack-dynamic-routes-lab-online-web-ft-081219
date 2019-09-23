c9304b0b0c2015366784072eefaca6f281f0834e
Laura Berge <lauraeberge@gmail.com>
2019-09-23 20:49:11 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,5 +1,17 @@
 class Application
   def call(env)
-    
+    def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+ 
+    if req.path=="/item"
+      resp.write "You requested the songs"
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+ 
+    resp.finish
+  end
   end
 end

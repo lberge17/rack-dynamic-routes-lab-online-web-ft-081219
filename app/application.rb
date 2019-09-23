3fb3ed163fac8ff158cb46389d76397c874e2dd5
Laura Berge <lauraeberge@gmail.com>
2019-09-23 21:06:08 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,20 +7,28 @@ class Application
  
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
+      item = @@items.find{|i| i.name == item_name
       
-      if @@items.include?(item_name)
-        item = @@items.find{|item| item.name = item_name}
+      if item
         resp.write item.price
-        
-        
-        song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
-        song = @@songs.find{|s| s.title == song_title}
- 
-        resp.write song.artist
       else
         resp.write "Item not found"
         resp.status 400
       end
+      
+      #if @@items.include?(item_name)
+       # item = @@items.find{|item| item.name = item_name}
+        #resp.write item.price
+        
+        
+        #song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
+        #song = @@songs.find{|s| s.title == song_title}
+ 
+        #resp.write song.artist
+      #else
+      #  resp.write "Item not found"
+       # resp.status 400
+      #end
       
     else
       resp.write "Route not found"
